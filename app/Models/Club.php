fb5ca87376a75ad0d01c3297258d732a5864bdce
Jack Ellis <jackellis1504@gmail.com>
2026-03-22 17:58:14 +0000

    clubs
     M database/seeders/DatabaseSeeder.php
     M stubs/migration.create.stub
    ?? app/Http/Controllers/ClubController.php
    ?? app/Http/Requests/StoreClubRequest.php
    ?? app/Http/Requests/UpdateClubRequest.php
    ?? app/Models/Club.php
    ?? app/Policies/
    ?? database/factories/ClubFactory.php
    ?? database/migrations/2026_03_22_175433_create_clubs_table.php
    ?? database/seeders/ClubSeeder.php

---
+++ app/Models/Club.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\ClubFactory;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+#[Fillable(['name', 'description'])]
+class Club extends Model
+{
+    /** @use HasFactory<ClubFactory> */
+    use HasFactory;
+
+    use HasUuids;
+    use SoftDeletes;
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            //
+        ];
+    }
+}

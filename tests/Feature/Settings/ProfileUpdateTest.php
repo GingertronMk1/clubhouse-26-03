7e80e89eb79520aa100369ae8e24110be983ee6c
Jack Ellis <jackellis1504@gmail.com>
2026-03-22 17:43:02 +0000

    amend test
     M tests/Feature/Settings/ProfileUpdateTest.php

--- tests/Feature/Settings/ProfileUpdateTest.php
+++ tests/Feature/Settings/ProfileUpdateTest.php
@@ -52,6 +52,7 @@ test('email verification status is unchanged when the email address is unchanged
 
 test('user can delete their account', function () {
     $user = User::factory()->create();
+    expect($user->deleted_at)->not->toBeNull();
 
     $response = $this
         ->actingAs($user)
@@ -64,11 +65,12 @@ test('user can delete their account', function () {
         ->assertRedirect(route('home'));
 
     $this->assertGuest();
-    expect($user->fresh())->toBeNull();
+    expect($user->deleted_at)->not->toBeNull();
 });
 
 test('correct password must be provided to delete account', function () {
     $user = User::factory()->create();
+    expect($user->deleted_at)->not->toBeNull();
 
     $response = $this
         ->actingAs($user)
@@ -81,5 +83,5 @@ test('correct password must be provided to delete account', function () {
         ->assertSessionHasErrors('password')
         ->assertRedirect(route('profile.edit'));
 
-    expect($user->fresh())->not->toBeNull();
+    expect($user->deleted_at)->not->toBeNull();
 });
